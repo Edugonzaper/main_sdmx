fad5ab1e82f87db68763bf96005ecc326ffd0899
enprava <epradavazquez@gmail.com>
2023-01-18 11:02:25 +0100

    Añadida la parte de CKAN

--- functions.py
+++ functions.py
@@ -1,6 +1,10 @@
+import os
+
 import pandas as pd
 import yaml
 from iecasdmx.ieca.actividad import Actividad
+from mdmpyclient.ckan.ckan import Ckan
+from mdmpyclient.mdm import MDM
 
 
 def execute_actividades(configuracion_ejecucion, configuracion_global, configuracion_actividades,
@@ -160,3 +164,27 @@ def script_provisional(cube_data, columns):
         if column not in cube_data.columns:
             cube_data.insert(len(cube_data.columns), column, '_Z')
     return cube_data
+
+
+def volcado_ckan(configuracion_global, traductor, controller):
+    ckan = Ckan(configuracion_global)
+    if configuracion_global['reset_ckan']:
+        ckan.datasets.remove_all_datasets()
+
+    if not controller:
+        controller = MDM(configuracion_global, traductor)  # Supongo que esta solucion es temporal.
+    for cube in controller.cubes.data:
+        first_ = cube.find('_')
+        second_ = cube.find('_', first_ + 1)
+        nombre_actividad = cube[first_ + 1:second_]
+        id_consulta = cube[second_ + 1:]
+        agencia = cube[:first_]
+        id_df = f'{agencia}_{nombre_actividad}_{id_consulta}'
+        id_dataset = f'{nombre_actividad}_{id_consulta}'
+        name_dataset = controller.dataflows.data[agencia][id_df]['1.0'].names['es']
+        ckan.datasets.create(id_dataset.lower(), name_dataset, ckan.orgs.orgs[nombre_actividad.lower()])
+        path = os.path.join(configuracion_global['directorio_datos'], nombre_actividad, 'procesados')
+        ckan.resources.create_from_file(f'{path}.{id_consulta}.csv', id_dataset, 'csv', id_dataset.lower())
+        ckan.resources.create_from_file(
+            f'{configuracion_global["sistema_informacion/metadatos_html"]}/REPORT_{nombre_actividad}.html',
+            nombre_actividad, 'html', id_dataset.lower())
